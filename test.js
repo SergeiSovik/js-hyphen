df084ed478b2145cc09f0bb3542353db99a97386
Sergio Rando <sovik.sergey.appledev@gmail.com>
2020-02-27 16:03:27 +0300

    Update

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Sergio Rando <segio.rando@yahoo.com>
+ * Copyright 2020 Sergei Sovik <sergeisovik@yahoo.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,6 @@ import { hyphen } from "./modules/hyphen.js"
 
 platform.console.log(hyphen('Example of License Hyphenation', '-'));
 
-platform.console.log(hyphen('Copyright 2020 Sergio Rando <segio.rando@yahoo.com>', '-'));
+platform.console.log(hyphen('Copyright 2020 Sergei Sovik <sergeisovik@yahoo.com>', '-'));
 platform.console.log(hyphen('Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at', '-'));
 platform.console.log(hyphen('Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.', '-'));
